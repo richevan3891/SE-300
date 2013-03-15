2b528b60a4869e99401ffe356b0a9fc8181ae120
Evan Richardson <richare1@my.erau.edu>
2013-03-15 14:51:08 -0400

    Updated All Files
    Did not do any additional coding, just combined all of the newest coding
    from all team members.

---
+++ Richardson_15Mar_Rev3/Route_findpath_dialogue.java
@@ -0,0 +1,159 @@
+// The purpose of this code is to provide the mechanism by which a
+// user can find a path in the ARP program. It occurs as a dialogue
+// box with several options.
+
+// Last edit: 8 March 2013
+
+
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+
+import javax.swing.*;
+
+public class Route_findpath_dialogue extends JFrame {
+	
+	// Initialize static labels
+	static private JLabel Carrierlabel = new JLabel("Carrier", SwingConstants.CENTER);
+	static private JLabel DepAPlabel = new JLabel("Departure Airport", SwingConstants.CENTER);
+	static private JLabel Deptimelabel = new JLabel("Departure Time", SwingConstants.CENTER);
+	static private JLabel ArrAPlabel = new JLabel("Arrival Airport", SwingConstants.CENTER);
+	static private JLabel Arrtimelabel = new JLabel("Arrival Time", SwingConstants.CENTER);
+	static private JLabel Optionslabel = new JLabel("Options", SwingConstants.CENTER);
+	
+	// Initialize check boxes
+	JCheckBox Checkdeptime = new JCheckBox();
+	JCheckBox Checkarrtime = new JCheckBox();
+	JCheckBox Checkcarrier = new JCheckBox();
+	
+	// Initialize the search route and cancel buttons and panel
+	JButton Findpathbutton = new JButton("Find Path");
+	JButton Cancelbutton = new JButton("Cancel");
+	JPanel Buttonpanel = new JPanel();
+
+	// Initialize choice panels
+	JPanel Choicepanel = new JPanel();
+	
+	JPanel Selectpanel = new JPanel();
+	JPanel DepAPpanel = new JPanel();
+	JPanel ArrAPpanel = new JPanel();
+	
+	JPanel Optionpanel = new JPanel();
+	JPanel Deptimepanel = new JPanel();
+	JPanel Deptimecheckpanel = new JPanel();
+	JPanel Arrtimepanel = new JPanel();
+	JPanel Arrtimecheckpanel = new JPanel();
+	JPanel Carrierpanel = new JPanel();
+	JPanel Carriercheckpanel = new JPanel();
+	
+	// Get the screen size
+	Dimension totalSize = Toolkit.getDefaultToolkit().getScreenSize();
+	int width = totalSize.width;
+	int height = totalSize.height;
+	
+	
+	
+	// Constructor for the dialogue box
+	public Route_findpath_dialogue(String[] Airports) {
+		
+		String[] Carriers = {"Delta", "United"};
+		
+		// Initialize all drop-down menus
+		JComboBox Carrierchoice = new JComboBox(Carriers);
+		JComboBox DepAPchoice = new JComboBox(Airports);
+		JComboBox ArrAPchoice = new JComboBox(Airports);
+		
+		TimeSelectBoxPanel Deptimechoice = new TimeSelectBoxPanel();
+		TimeSelectBoxPanel Arrtimechoice = new TimeSelectBoxPanel();
+		
+		// Create departure and arrival airport panels
+		DepAPpanel.add(DepAPlabel);
+		DepAPpanel.add(DepAPchoice);
+		DepAPpanel.setLayout(new BoxLayout(DepAPpanel, BoxLayout.Y_AXIS));
+		DepAPpanel.setAlignmentY(CENTER_ALIGNMENT);
+		
+		ArrAPpanel.add(ArrAPlabel);
+		ArrAPpanel.add(ArrAPchoice);
+		ArrAPpanel.setLayout(new BoxLayout(ArrAPpanel, BoxLayout.Y_AXIS));
+		ArrAPpanel.setAlignmentY(CENTER_ALIGNMENT);
+		
+		// Create departure time panel with check box panel
+		Deptimepanel.add(Deptimelabel);
+		Deptimepanel.add(Deptimechoice);
+		Deptimepanel.setLayout(new BoxLayout(Deptimepanel, BoxLayout.Y_AXIS));
+		Deptimepanel.setAlignmentY(CENTER_ALIGNMENT);
+		Deptimecheckpanel.add(Deptimepanel);
+		Deptimecheckpanel.add(Checkdeptime);
+		Deptimecheckpanel.setLayout(new BoxLayout(Deptimecheckpanel, BoxLayout.X_AXIS));
+		Deptimecheckpanel.setAlignmentX(CENTER_ALIGNMENT);
+		
+		// Create arrival time panel with check box panel
+		Arrtimepanel.add(Arrtimelabel);
+		Arrtimepanel.add(Arrtimechoice);
+		Arrtimepanel.setLayout(new BoxLayout(Arrtimepanel, BoxLayout.Y_AXIS));
+		Arrtimepanel.setAlignmentY(CENTER_ALIGNMENT);
+		Arrtimecheckpanel.add(Arrtimepanel);
+		Arrtimecheckpanel.add(Checkarrtime);
+		Arrtimecheckpanel.setLayout(new BoxLayout(Arrtimecheckpanel, BoxLayout.X_AXIS));
+		Arrtimecheckpanel.setAlignmentX(CENTER_ALIGNMENT);
+		
+		// Create carrier panel with check box panel
+		Carrierpanel.add(Carrierlabel);
+		Carrierpanel.add(Carrierchoice);
+		Carrierpanel.setLayout(new BoxLayout(Carrierpanel, BoxLayout.Y_AXIS));
+		Carrierpanel.setAlignmentY(CENTER_ALIGNMENT);
+		Carriercheckpanel.add(Carrierpanel);
+		Carriercheckpanel.add(Checkcarrier);
+		Carriercheckpanel.setLayout(new BoxLayout(Carriercheckpanel, BoxLayout.X_AXIS));
+		Carriercheckpanel.setAlignmentX(CENTER_ALIGNMENT);
+		
+		// Add all selection panels and option panels to the overall choice panel
+		Selectpanel.setLayout(new BoxLayout(Selectpanel, BoxLayout.Y_AXIS));
+		Selectpanel.setAlignmentY(CENTER_ALIGNMENT);
+		Selectpanel.add(DepAPpanel);
+		Selectpanel.add(ArrAPpanel);
+		
+		Optionpanel.setLayout(new BoxLayout(Optionpanel, BoxLayout.Y_AXIS));
+		Optionpanel.setAlignmentY(CENTER_ALIGNMENT);
+		Optionpanel.add(Optionslabel);
+		Optionpanel.add(Box.createVerticalGlue());
+		Optionpanel.add(Deptimecheckpanel);
+		Optionpanel.add(Arrtimecheckpanel);
+		Optionpanel.add(Carriercheckpanel);
+		
+		Choicepanel.setLayout(new BoxLayout(Choicepanel, BoxLayout.X_AXIS));
+		Choicepanel.setAlignmentY(CENTER_ALIGNMENT);
+		Choicepanel.add(Selectpanel);
+		Choicepanel.add(Box.createHorizontalGlue());
+		Choicepanel.add(Optionpanel);
+		
+		// Add the buttons to the overall button panel
+		Buttonpanel.setLayout(new FlowLayout());
+		Buttonpanel.add(Cancelbutton);
+		Buttonpanel.add(Findpathbutton);
+		
+		// Add the choice panel and button to the dialogue box
+		setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
+		add(Choicepanel);
+		add(Buttonpanel);
+		
+	}
+	
+	
+	// Method for displaying the dialogue box
+	public void showDialogue(){
+		
+		this.setTitle("Find Path");								// Rename the title
+		this.setSize((width * 4 / 10), (height * 4 / 10));		// Set the size
+		this.setResizable(false);								// No resizing
+		this.setLocationRelativeTo(null);						// Center it
+		this.setVisible(true);									// Make it visible
+		
+	}
+	
+	
+	
+	
+	
+}
---
+++ Richardson_15Mar_Rev3/Route_searchroute_dialogue.java
@@ -0,0 +1,190 @@
+// The purpose of this code is to provide the mechanism by which a
+// user can search for a route in the ARP program. It occurs as a dialogue
+// box with several options.
+
+// Last edit: 8 March 2013
+
+
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+
+import javax.swing.*;
+
+public class Route_searchroute_dialogue extends JFrame {
+	
+	// Initialize static labels
+	static private JLabel Routenumlabel = new JLabel("Route Number", SwingConstants.CENTER);
+	static private JLabel Carrierlabel = new JLabel("Carrier", SwingConstants.CENTER);
+	static private JLabel DepAPlabel = new JLabel("Departure Airport", SwingConstants.CENTER);
+	static private JLabel Deptimelabel = new JLabel("Departure Time", SwingConstants.CENTER);
+	static private JLabel ArrAPlabel = new JLabel("Arrival Airport", SwingConstants.CENTER);
+	static private JLabel Arrtimelabel = new JLabel("Arrival Time", SwingConstants.CENTER);
+	
+	// Initialize check boxes
+	JCheckBox Checkroutenum = new JCheckBox();
+	JCheckBox CheckdepAP = new JCheckBox();
+	JCheckBox CheckarrAP = new JCheckBox();
+	JCheckBox Checkdeptime = new JCheckBox();
+	JCheckBox Checkarrtime = new JCheckBox();
+	JCheckBox Checkcarrier = new JCheckBox();
+	
+	// Initialize the search route and cancel buttons and panel
+	JButton Searchroutebutton = new JButton("Search");
+	JButton Cancelbutton = new JButton("Cancel");
+	JPanel Buttonpanel = new JPanel();
+
+	// Initialize choice panels
+	JPanel Choicepanel = new JPanel();
+	
+	JPanel Routenumpanel = new JPanel();
+	JPanel Routenumcheckpanel = new JPanel();
+	
+	JPanel DepAPpanel = new JPanel();
+	JPanel DepAPcheckpanel = new JPanel();
+	JPanel ArrAPpanel = new JPanel();
+	JPanel ArrAPcheckpanel = new JPanel();
+	
+	JPanel Deptimepanel = new JPanel();
+	JPanel Deptimecheckpanel = new JPanel();
+	JPanel Arrtimepanel = new JPanel();
+	JPanel Arrtimecheckpanel = new JPanel();
+	
+	JPanel Carrierpanel = new JPanel();
+	JPanel Carriercheckpanel = new JPanel();
+	
+	// Get the screen size
+	Dimension totalSize = Toolkit.getDefaultToolkit().getScreenSize();
+	int width = totalSize.width;
+	int height = totalSize.height;
+	
+	
+	
+	// Constructor for the dialogue box
+	public Route_searchroute_dialogue(String[] Airports, String[] Routenums) {
+		
+		String[] Carriers = {"Delta", "United"};
+		JComboBox Routenumchoice = new JComboBox(Routenums);
+		
+		// Initialize all drop-down menus
+		JComboBox Carrierchoice = new JComboBox(Carriers);
+		JComboBox DepAPchoice = new JComboBox(Airports);
+		JComboBox ArrAPchoice = new JComboBox(Airports);
+		
+		TimeSelectBoxPanel Deptimechoice = new TimeSelectBoxPanel();
+		TimeSelectBoxPanel Arrtimechoice = new TimeSelectBoxPanel();
+		
+		
+		// Try Overall Gridpanel
+		Choicepanel.setLayout(new GridLayout(6,3));
+		Choicepanel.add(Checkroutenum);
+		Checkroutenum.setAlignmentX(RIGHT_ALIGNMENT);
+		Choicepanel.add(Routenumlabel);
+		Choicepanel.add(Routenumchoice);
+		
+		Choicepanel.add(CheckdepAP);
+		Choicepanel.add(DepAPlabel);
+		Choicepanel.add(DepAPchoice);
+		
+		Choicepanel.add(CheckarrAP);
+		Choicepanel.add(ArrAPlabel);
+		Choicepanel.add(ArrAPchoice);
+		
+		Choicepanel.add(Checkdeptime);
+		Choicepanel.add(Deptimelabel);
+		Choicepanel.add(Deptimechoice);
+		
+		Choicepanel.add(Checkarrtime);
+		Choicepanel.add(Arrtimelabel);
+		Choicepanel.add(Arrtimechoice);
+		
+		Choicepanel.add(Checkcarrier);
+		Choicepanel.add(Carrierlabel);
+		Choicepanel.add(Carrierchoice);
+		
+		/*
+		// Create all choice panels and combine with checkpanels
+		Routenumpanel.setLayout(new BoxLayout(Routenumpanel, BoxLayout.X_AXIS));
+		Routenumpanel.setAlignmentY(CENTER_ALIGNMENT);
+		Routenumpanel.add(Routenumlabel);
+		//Routenumpanel.add(Box.createHorizontalBox());
+		Routenumpanel.add(Routenumchoice);
+		Routenumcheckpanel.add(Checkroutenum);
+		//Routenumcheckpanel.add(Box.createHorizontalGlue());
+		Routenumcheckpanel.add(Routenumpanel);
+		
+		DepAPpanel.add(DepAPlabel);
+		DepAPpanel.add(DepAPchoice);
+		DepAPpanel.setLayout(new BoxLayout(DepAPpanel, BoxLayout.X_AXIS));
+		DepAPpanel.setAlignmentY(CENTER_ALIGNMENT);
+		DepAPcheckpanel.add(CheckdepAP);
+		DepAPcheckpanel.add(DepAPpanel);
+		
+		ArrAPpanel.add(ArrAPlabel);
+		ArrAPpanel.add(ArrAPchoice);
+		ArrAPpanel.setLayout(new BoxLayout(ArrAPpanel, BoxLayout.X_AXIS));
+		ArrAPpanel.setAlignmentY(CENTER_ALIGNMENT);
+		ArrAPcheckpanel.add(CheckarrAP);
+		ArrAPcheckpanel.add(ArrAPpanel);
+		
+		Deptimepanel.add(Deptimelabel);
+		Deptimepanel.add(Deptimechoice);
+		Deptimepanel.setLayout(new BoxLayout(Deptimepanel, BoxLayout.X_AXIS));
+		Deptimepanel.setAlignmentY(CENTER_ALIGNMENT);
+		Deptimecheckpanel.add(Checkdeptime);
+		Deptimecheckpanel.add(Deptimepanel);
+		
+		Arrtimepanel.add(Arrtimelabel);
+		Arrtimepanel.add(Arrtimechoice);
+		Arrtimepanel.setLayout(new BoxLayout(Arrtimepanel, BoxLayout.X_AXIS));
+		Arrtimepanel.setAlignmentY(CENTER_ALIGNMENT);
+		Arrtimecheckpanel.add(Checkarrtime);
+		Arrtimecheckpanel.add(Arrtimepanel);
+		
+		Carrierpanel.add(Carrierlabel);
+		Carrierpanel.add(Carrierchoice);
+		Carrierpanel.setLayout(new BoxLayout(Carrierpanel, BoxLayout.X_AXIS));
+		Carrierpanel.setAlignmentY(CENTER_ALIGNMENT);
+		Carriercheckpanel.add(Checkcarrier);
+		Carriercheckpanel.add(Carrierpanel);
+				
+		// Add all checkpanels to the overall choice panel
+		Choicepanel.setLayout(new BoxLayout(Choicepanel, BoxLayout.Y_AXIS));
+		Choicepanel.setAlignmentY(CENTER_ALIGNMENT);
+		Choicepanel.add(Routenumcheckpanel);
+		Choicepanel.add(DepAPcheckpanel);
+		Choicepanel.add(ArrAPcheckpanel);
+		Choicepanel.add(Deptimecheckpanel);
+		Choicepanel.add(Arrtimecheckpanel);
+		Choicepanel.add(Carriercheckpanel);
+		*/
+		
+		// Add the buttons to the overall button panel
+		Buttonpanel.setLayout(new FlowLayout());
+		Buttonpanel.add(Cancelbutton);
+		Buttonpanel.add(Searchroutebutton);
+		
+		// Add the choice panel and button to the dialogue box
+		setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
+		add(Choicepanel);
+		add(Buttonpanel);
+		
+	}
+	
+	
+	// Method for displaying the dialogue box
+	public void showDialogue(){
+		
+		this.setTitle("Search Route");							// Rename the title
+		this.setSize((width * 2 / 10), (height * 4 / 10));		// Set the size
+		this.setResizable(false);								// No resizing
+		this.setLocationRelativeTo(null);						// Center it
+		this.setVisible(true);									// Make it visible
+		
+	}
+	
+	
+	
+	
+}
